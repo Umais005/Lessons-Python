0d90b72a22e53cc45a11b8a9fe9ce5501d22b1ef
Muhammad Umais <umaismuhammad82@gmail.com>
2026-06-28 18:39:47 +0500

    Lesson 1 completed

---
+++ Day-1/day_1.py
@@ -0,0 +1,23 @@
+# Operations
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3 % 4)
+print(3**4)
+print(3//4)
+print(abs(2+3))
+
+# Strings
+print('''My name is Umais, I am enjoying these lessons''')
+
+# Check data types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['meow', 'woof', "quack"]))
+print(type('Umais'))
+print(type(('guy', 'girl')))
+print(type({3.54, 1.3, 5, 4}))
+print(type({'Name': 'Umais'}))
